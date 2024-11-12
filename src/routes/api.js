2ffc2fa693af65c05a8590ba9b2894469efa99d0
thienbt03@gmail.com <thienbt05@gmail.com>
2024-11-12 15:35:57 +0700

    step 5

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
-const { createUser } = require('../controllers/userController');
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const { createUser, handleLogin } = require("../controllers/userController");
 
 const routerAPI = express.Router();
 
+routerAPI.get("/", (req, res) => {
+  return res.status(200).json("Hello world API");
+});
 
-routerAPI.get("/",(req, res) => {
-  return res.status(200).json("Hello world API")
-})
-
-routerAPI.post("/register", createUser)
+routerAPI.post("/register", createUser);
+routerAPI.post("/login", handleLogin);
 module.exports = routerAPI; //export default
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,25 +1,54 @@
 const User = require("../models/user");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 const createUserService = async (name, email, password) => {
-    try {
-        //hash user password
-        const hashPassword = await bcrypt.hash(password,saltRounds)
-        //save user to database
-        let result = await User.create({
-            name: name,
-            email: email,
-            password: hashPassword,
-            role: "T1"
-        })
-        return result;
+  try {
+    //hash user password
+    const hashPassword = await bcrypt.hash(password, saltRounds);
+    //save user to database
+    let result = await User.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+      role: "T1",
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
-    } catch (error) {
-        console.log(error);
-        return null;
+const loginService = async (email1, password) => {
+  try {
+    //fetch user by email
+    const user = await User.findOne({ email: email1 });
+    if (user) {
+      //compare password
+      console.log(">>check user: ", user);
+      const isMatchPassword = await bcrypt.compare(password, user.password);
+      if (!isMatchPassword) {
+        return {
+          EC: 2,
+          EM: "Email/Password khong hop le",
+        };
+      } else {
+        //create an access token
+        return "create an access token";
+      }
+    } else {
+      return {
+        EC: 1,
+        EM: "Email/Password khong hop le",
+      };
     }
-}
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 module.exports = {
-    createUserService
-}
+  createUserService,
+  loginService,
+};
